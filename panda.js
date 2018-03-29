408c0f8376759de033c3df2fdf4776e3fdaffcf7
“ <guanbobo0@gmail.com>
2018-03-29 15:01:05 +0800

    增加使用bower,并修改默认下载位置

--- panda.js
+++ panda.js
@@ -18,6 +18,12 @@ app.set('view engine', 'pug');
 // 静态资源
 app.use(express.static(__dirname + '/public'));
 
+// 测试框架mocha
+app.use((req, res, next) => {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === "1";
+	next();
+})
+
 // 首页
 app.get('/', (req, res, next) => {
 	res.render('home');
